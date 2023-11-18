111d9c28f1e7e26305dc28e0ae0cd11576a5f28d
Rezan İçgil <rezanicgil@gmail.com>
2023-11-18 04:52:05 +0300

    feat: get a task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 
@@ -9,5 +9,21 @@ export class TasksController {
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    console.log('Title: ', title);
+    console.log('Description: ', description);
+
+    return this.tasksService.createTask(title, description);
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id): Task {
+    return this.tasksService.getTaskById(id);
   }
 }
