e2d0a58e78754ffeedd19ecdb4110d1bc8c4bd7c
Rezan İçgil <rezanicgil@gmail.com>
2023-11-18 23:29:59 +0300

    feat: filter and search validations

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,11 @@
+import { IsEnum, IsOptional } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
+  @IsEnum(TaskStatus)
+  @IsOptional()
   status?: TaskStatus;
+
+  @IsOptional()
   search?: string;
 }
