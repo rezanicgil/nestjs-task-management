a9ef87ba9881142a440bfa058dd230c271d7c274
Rezan İçgil <rezanicgil@gmail.com>
2023-11-18 23:26:27 +0300

    feat: validations update-task

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,7 +1,14 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class UpdateTaskDto {
+  @IsString()
   title: string;
+
+  @IsString()
   description: string;
+
+  @IsEnum(TaskStatus)
+  @IsOptional()
   status: TaskStatus;
 }
