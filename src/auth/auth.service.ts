39bf6f38a17e755ce033f680770fbbd1b0c4b9b4
Rezan İçgil <rezanicgil@gmail.com>
2023-11-19 20:54:43 +0300

    feat: error handling user conflicts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
@@ -14,6 +18,15 @@ export class AuthService {
     const { username, password } = authCredentialsDto;
 
     const user = this.userRepository.create({ username, password });
-    await this.userRepository.save(user);
+
+    try {
+      await this.userRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
