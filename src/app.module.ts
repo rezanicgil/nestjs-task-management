641dfabcbcde89f81369c1e7cafda8983f0e0327
Rezan İçgil <rezanicgil@gmail.com>
2023-11-22 16:14:00 +0300

    feat: config validation schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { DataSource } from 'typeorm';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: [`.env.stage.${process.env.STAGE}`] }),
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
+    }),
     DatabaseModule,
     TasksModule,
     AuthModule,
---
+++ src/config.schema.ts
@@ -0,0 +1,13 @@
+import * as Joi from '@hapi/joi';
+
+export const configValidationSchema = Joi.object({
+  STAGE: Joi.string().required(),
+  PORT: Joi.number().default(3000).required(),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().default(5432).required(),
+  DB_USER: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_NAME: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
+  JWT_EXPIRES_IN: Joi.string().required(),
+});
