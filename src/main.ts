849fd078e3b3df3b502d262cbb415a6182b6f23d
Rezan İçgil <rezanicgil@gmail.com>
2023-11-22 22:31:32 +0300

    fix: cors enabled to connect clients to the backnd

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const configService: ConfigService = new ConfigService();
 
   app.useGlobalPipes(new ValidationPipe());
